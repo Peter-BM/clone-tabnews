90cbff2971a545b3e24269287f292eb2075f1ec8
Koko <pedrobobella@gmail.com>
2025-01-23 19:49:39 -0300

    feat: Adds pages directory to the project.

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() { 
+  return <h1>Oi Amor, percebi que mesmo sendo um programador a algum tempo nunca tinha 
+    feito uma página web pra você então eu gostaria que soubesse que <strong>EU TE AMO!❤️</strong>
+    </h1>
+}
+
+export default Home;
