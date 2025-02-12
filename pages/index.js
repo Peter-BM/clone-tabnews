db0919d28106b42a1c3a469adffc051eb0cc9ef0
Koko <pedrobobella@gmail.com>
2025-02-11 21:22:18 -0300

    feat: Changes the main message in the menu

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() { 
-  return <h1>Oi Amor, percebi que mesmo sendo um programador a algum tempo nunca tinha 
-    feito uma página web pra você então eu gostaria que soubesse que <strong>EU TE AMO!❤️</strong>
-    </h1>
+  return <h1>Esse título é diferente do anterior</h1>
 }
 
 export default Home;
