9ff877e5e376861fcab3834a3bea48241643d163
Koko <pedrobobella@gmail.com>
2025-06-16 21:23:53 -0300

    raw: add  and  scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 This project is being created to test and improve my programming skills. If you come across it, feel free to suggest improvements.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home() { 
-  return <h1>Esse título é diferente do anterior</h1>
+function Home() {
+  return <h1>Esse título é diferente do anterior</h1>;
 }
 
 export default Home;
